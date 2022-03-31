3d7f61d965951b77dfdc1d99bf383952456833fb
Bo-Shang-Chao <seanbopo@gmail.com>
2022-03-31 14:32:31 +0800

    4 files modified delete Hero

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -79,4 +79,15 @@ export class HeroService {
           .pipe(tap((newHero: Hero) => this.log(`added hero w/ id=${newHero.id}`)),
           catchError(this.handleError<Hero>(`addHero`)));
   }
+
+  /** DELETE: delete the hero from the server */
+  deleteHero(id: number): Observable<Hero> {
+    const url = `${this.heroesUrl}/${id}`;
+
+    return this.http.delete<Hero>(url, this.httpOptions)
+    .pipe(
+      tap(_ => this.log(`deleted hero id=${id}`)),
+      catchError(this.handleError<Hero>(`deleteHero`))
+      );
+  }
 }
