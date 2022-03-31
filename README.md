f28f22d8f9df588d44509971bd86f4be1909612b
Bo-Shang-Chao <seanbopo@gmail.com>
2022-03-31 10:27:04 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-hero-http-hsiang
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-ivy-herodsssa-fpdsyc)
---
+++ src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+// import { CommonModule } from '@angular/common';
+
+import { AppComponent } from './app.component';
+import { HelloComponent } from './hello.component';
+import { HeroesComponent } from './heroes/heroes.component';
+import { HeroDetailComponent } from './hero-detail/hero-detail.component';
+import { MessagesComponent } from './messages/messages.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+
+import { AppRoutingModule } from './app-routing.module';
+
+@NgModule({
+  imports:      [ BrowserModule, FormsModule, AppRoutingModule ],
+  declarations: [ AppComponent, HelloComponent, HeroesComponent,HeroDetailComponent, MessagesComponent, DashboardComponent ],
+  bootstrap:    [ AppComponent ]
+})
+export class AppModule { }
